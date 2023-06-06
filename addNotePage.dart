f9613b9cca296b087904c880f4ebc5b3b2f7fde1
AnhKhoa2311 <133756374+AnhKhoa2311@users.noreply.github.com>
2023-06-06 23:38:46 +0700

    Update addNotePage.dart

--- addNotePage.dart
+++ addNotePage.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_noteapp/model/note_model.dart';
+import 'package:flutter_noteapp/ui/noteapp/note_view_viewmodel.dart';
 import 'package:flutter_noteapp/ui/noteapp/notescreen.dart';
 
 class addNotePage extends StatefulWidget {
@@ -9,21 +12,35 @@ class addNotePage extends StatefulWidget {
 }
 
 class _addNotePageState extends State<addNotePage> {
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  late DatabaseReference dbRef;
+  // Note_Model noteModel = Note_Model.withArgs(id: 0, title: '', description: '');
+  // Note_View_Model model = Note_View_Model();
+
+  @override
+  void initState() {
+    super.initState();
+    dbRef = FirebaseDatabase.instance.ref().child('Notes');
+  }
+
   @override
   Widget build(BuildContext context) {
+    // _titleController.text = noteModel.title;
+    // _descriptionController.text = noteModel.description;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.blue,
         title: const Text(
           'Add Note',
-          style: TextStyle(color: Colors.lightBlueAccent),
+          style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
         leading: InkWell(
           onTap: () {
             Navigator.pop(context);
           },
-          child: Center(
+          child: const Center(
             child: Text(
               'Cancel',
               style: TextStyle(
@@ -39,19 +56,35 @@ class _addNotePageState extends State<addNotePage> {
                 'Save',
                 style: TextStyle(
                     fontSize: 17,
-                    color: Colors.lightBlueAccent,
+                    color: Colors.tealAccent,
                     fontWeight: FontWeight.bold),
               ),
             ),
+            // onTap: () {
+            //   final snackbar = SnackBar(
+            //     content: const Text('Save Completed !'),
+            //     action: SnackBarAction(
+            //       label: 'Undo',
+            //       onPressed: () {},
+            //     ),
+            //   );
+            //   ScaffoldMessenger.of(context).showSnackBar(snackbar);
+            // },
             onTap: () {
-              final snackbar = SnackBar(
-                content: const Text('Save Completed !'),
-                action: SnackBarAction(
-                  label: 'Undo',
-                  onPressed: () {},
-                ),
-              );
-              ScaffoldMessenger.of(context).showSnackBar(snackbar);
+              // noteModel ??= Note_Model(title: '', description: '');
+
+              // Navigator.pop(context);
+              // noteModel.title = _titleController.text.trim();
+              // noteModel.description = _descriptionController.text.trim();
+              // model.save(noteModel);
+              Map<String, String> notes = {
+                'title': _titleController.text,
+                'description': _descriptionController.text
+              };
+              dbRef.push().set(notes);
+              Navigator.pop(context);
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text('Save sucess!')));
             },
           )
         ],
@@ -59,6 +92,7 @@ class _addNotePageState extends State<addNotePage> {
       body: Column(
         children: [
           TextField(
+            controller: _titleController,
             keyboardType: TextInputType.text,
             decoration: InputDecoration(
                 hintText: 'Title', border: OutlineInputBorder()),
@@ -67,13 +101,14 @@ class _addNotePageState extends State<addNotePage> {
             height: 35,
           ),
           TextField(
+            controller: _descriptionController,
             maxLines: 5,
             keyboardType: TextInputType.text,
             decoration: InputDecoration(
               hintText: 'Write a note',
               border: OutlineInputBorder(),
             ),
-          )
+          ),
         ],
       ),
     );
